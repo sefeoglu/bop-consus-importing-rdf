e44ad9548f4ab5ea549144d517798cf7a8c22a58
Simon Dutkowski <simon.dutkowski@fokus.fraunhofer.de>
2019-01-10 11:19:45 +0100

    gitlab-ci tags cleanup

--- src/test/java/io/piveau/importing/ImportingTest.java
+++ src/test/java/io/piveau/importing/ImportingTest.java
@@ -3,6 +3,7 @@ package io.piveau.importing;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +11,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 @DisplayName("Testing the importer")
@@ -22,39 +19,30 @@ public class ImportingTest {
 
     @BeforeEach
     public void startImporter(Vertx vertx, VertxTestContext testContext) {
-        vertx.deployVerticle(new MainVerticle(), testContext.succeeding(response -> testContext.verify(testContext::completeNow)));
+        vertx.deployVerticle(new MainVerticle(), testContext.completing());
     }
 
     @Test
     @DisplayName("pipe receiving")
+    @Timeout(value = 5, timeUnit = TimeUnit.MINUTES)
     public void sendPipe(Vertx vertx, VertxTestContext testContext) {
-        Path path = FileSystems.getDefault().getPath("src/test/resources/test-pipe.json");
-        try {
-            String pipeString = new String(Files.readAllBytes(path));
-            JsonObject pipe = new JsonObject(pipeString);
-            WebClient client = WebClient.create(vertx);
-            client.post(8080, "localhost", "/pipe")
-                .putHeader("content-type", "application/json")
-                .sendJsonObject(pipe, response -> {
-                    if (response.succeeded()) {
-                        if (response.result().statusCode() == 202) {
+        vertx.fileSystem().readFile("src/test/resources/test-pipe.json", result -> {
+            if (result.succeeded()) {
+                JsonObject pipe = new JsonObject(result.result());
+                WebClient client = WebClient.create(vertx);
+                client.post(8080, "localhost", "/pipe")
+                        .putHeader("content-type", "application/json")
+                        .sendJsonObject(pipe, testContext.succeeding(response -> testContext.verify(() -> {
+                            if (response.statusCode() == 202) {
 //                            testContext.completeNow();
-                        } else {
-                            testContext.failNow(new Throwable(response.result().statusMessage()));
-                        }
-                    } else {
-                        testContext.failNow(response.cause());
-                    }
-                });
-        } catch (IOException e) {
-            testContext.failNow(e);
-        }
-
-        try {
-            testContext.awaitCompletion(5, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+                            } else {
+                                testContext.failNow(new Throwable(response.statusMessage()));
+                            }
+                        })));
+            } else {
+                testContext.failNow(result.cause());
+            }
+        });
     }
 
 }
