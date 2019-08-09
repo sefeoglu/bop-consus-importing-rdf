40e7da766ad1f1330378c7c6f723ff8040cc8097
Simon Dutkowski <simon.dutkowski@fokus.fraunhofer.de>
2019-08-09 11:56:54 +0200

    remove canonical hash from dataInfo

--- src/main/java/io/piveau/importing/rdf/ImportingRdfVerticle.java
+++ src/main/java/io/piveau/importing/rdf/ImportingRdfVerticle.java
@@ -4,22 +4,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.piveau.pipe.connector.PipeContext;
-import io.piveau.utils.Hash;
 import io.piveau.utils.HydraPaging;
 import io.piveau.utils.JenaUtils;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
-import org.apache.jena.graph.Node;
-import org.apache.jena.graph.Triple;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.vocabulary.DCAT;
@@ -40,7 +37,7 @@ public class ImportingRdfVerticle extends AbstractVerticle {
     private int defaultDelay;
 
     @Override
-    public void start(Future<Void> startFuture) {
+    public void start(Promise<Void> startPromise) {
         vertx.eventBus().consumer(ADDRESS, this::handlePipe);
         client = WebClient.create(vertx);
 
@@ -51,9 +48,9 @@ public class ImportingRdfVerticle extends AbstractVerticle {
         retriever.getConfig(ar -> {
             if (ar.succeeded()) {
                 defaultDelay = ar.result().getInteger("PIVEAU_IMPORTING_SEND_LIST_DELAY", 8000);
-                startFuture.complete();
+                startPromise.complete();
             } else {
-                startFuture.fail(ar.cause());
+                startPromise.fail(ar.cause());
             }
         });
     }
@@ -108,8 +105,8 @@ public class ImportingRdfVerticle extends AbstractVerticle {
                                     .put("total", hydra.getTotal() != 0 ? hydra.getTotal() : datasets.size())
                                     .put("counter", identifiers.size())
                                     .put("identifier", identifier)
-                                    .put("catalogue", config.path("catalogue").asText())
-                                    .put("hash", JenaUtils.canonicalHash(model));
+//                                    .put("hash", JenaUtils.canonicalHash(model))
+                                    .put("catalogue", config.path("catalogue").asText());
                             pipeContext.setResult(pretty, outputFormat, dataInfo).forward(client);
                             pipeContext.log().info("Data imported: {}", dataInfo);
                             pipeContext.log().debug("Data content: {}", pretty);
